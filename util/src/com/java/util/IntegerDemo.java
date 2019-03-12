bc6674371376ffd7e0721ec10a7187860078ca39
zhangpanhua6616 <19825292682@139.com>
2019-03-12 09:52:53 +0800

    进制转换

---
+++ util/src/com/java/util/IntegerDemo.java
@@ -0,0 +1,32 @@
+package com.java.util;
+
+/**
+ * Integer类的练习
+ * @author 张攀华
+ * 2019.3.12
+ */
+public class IntegerDemo {
+    public static void main(String[] args) {
+        int num = 31;
+        String string2 = Integer.toBinaryString(num);
+        String string3 = Integer.toHexString(num);
+        System.out.println("二进制："+string2);
+        System.out.println("十六进制："+string3);
+        System.out.println("-----------分割线，以下是自定义方法");
+        System.out.println("转换二进制："+convert(31,2));
+    }
+    //编写代码将num转换成radix进制
+    private static String convert(int num, int radix){
+        StringBuffer stringBuffer = new StringBuffer();
+                while(num !=0){
+                    //定义remainder为余数
+                    int remainder = num % radix;
+                    //将余数加入stringBuffer中
+                    stringBuffer.append(String.valueOf(remainder));
+                    //更新被除数为商
+                    num = num / radix;
+                }
+                //反转字符串
+                return stringBuffer.reverse().toString();
+    }
+}
